da699652b9afbc6179770581eb9924025ecd177d
Bryan Bishop <kanzure@pikachu.austin.rr.com>
2009-07-05 12:24:13 -0500

    formatting in views.py

--- wiki/views.py
+++ wiki/views.py
@@ -15,24 +15,31 @@ from git import *
 def index(request):
     returnstring = "this is the index"
     return HttpResponse(returnstring)
+
 def edit(request, path=""):
     returnstring = "edit (path=%s)" % (path)
     return HttpResponse(returnstring)
+
 def indexviewcommit(request,sha=""):
     returnstring = "indexviewcommit (sha=%s)" % (sha)
     return HttpResponse(returnstring)
+
 def archive(request,path=""):
     returnstring = "archive (path=%s)" % (path)
     return HttpResponse(returnstring)
+
 def history(request,path=""):
     returnstring = "history (path=%s)" % (path)
     return HttpResponse(returnstring)
+
 def diff(request, path=""):
     returnstring = "diff (path=%s)" % (path)
     return HttpResponse(returnstring)
+
 def upload(request, path=""):
     returnstring = "upload (path=%s)" % (path)
     return HttpResponse(returnstring)
+
 def new(request,path=""):
     returnstring = "new (path=%s)" % (path)
     return HttpResponse(returnstring)
@@ -43,6 +50,7 @@ def viewcommit_for_file(request,path="",sha=""):
 def changelog(request,path=""):
     returnstring = "changelog (path=%s)" % (path)
     return HttpResponse(returnstring)
+
 def view(request,path=""):
     returnstring = "view (path=%s)" % (path)
     return HttpResponse(returnstring)
