55b2eeeb495603a5af2617f725152a3b9cf8266b
Bryan Bishop <kanzure@pikachu.austin.rr.com>
2009-07-05 10:52:26 -0500

    adding the rest of the wiki dir

--- wiki/views.py
+++ wiki/views.py
@@ -7,35 +7,32 @@ def index(request):
     returnstring = "this is the index"
     return HttpResponse(returnstring)
 def edit(request, path=""):
-    returnstring = "edit"
+    returnstring = "edit (path=%s)" % (path)
     return HttpResponse(returnstring)
 def indexviewcommit(request,sha=""):
-    returnstring = "indexviewcommit"
+    returnstring = "indexviewcommit (sha=%s)" % (sha)
     return HttpResponse(returnstring)
 def archive(request,path=""):
-    returnstring = "archive"
+    returnstring = "archive (path=%s)" % (path)
     return HttpResponse(returnstring)
 def history(request,path=""):
-    returnstring = "history"
+    returnstring = "history (path=%s)" % (path)
     return HttpResponse(returnstring)
 def diff(request, path=""):
-    returnstring = "diff"
+    returnstring = "diff (path=%s)" % (path)
     return HttpResponse(returnstring)
 def upload(request, path=""):
-    returnstring = "upload"
+    returnstring = "upload (path=%s)" % (path)
     return HttpResponse(returnstring)
 def new(request,path=""):
-    returnstring = "new"
+    returnstring = "new (path=%s)" % (path)
     return HttpResponse(returnstring)
 def viewcommit_for_file(request,path="",sha=""):
-    returnstring = "viewcommit_for_file"
+    returnstring = "viewcommit_for_file (path=%s,sha=%s)" % (path,sha)
     return HttpResponse(returnstring)
-def changelog(request):
-    returnstring = "changelog"
-    return HttpResponse(returnstring)
-def changelog_for_file(request,path=""):
-    returnstring = "changelog_for_file"
+def changelog(request,path=""):
+    returnstring = "changelog (path=%s)" % (path)
     return HttpResponse(returnstring)
 def view(request,path=""):
-    returnstring = "view"
+    returnstring = "view (path=%s)" % (path)
     return HttpResponse(returnstring)
