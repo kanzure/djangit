e0e62700489e4b869ee2ce45425e6cce81ffd1f0
Bryan Bishop <kanzure@pikachu.austin.rr.com>
2009-07-05 09:42:15 -0500

    forgot something stupid in urls.py

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+import pydjangitwiki.wiki
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -23,24 +24,24 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     #login/signup stuff not included here
     #also user profile stuff not included here
-    (r'^edit/', 'pydjangitwiki.wiki.edit'),
-    (r'^root$', 'pydjangitwiki.wiki.index'),
-    (r'^/$', 'pydjangitwiki.wiki.index'),
+    (r'^edit/', 'pydjangitwiki.wiki.views.edit'),
+    (r'^root$', 'pydjangitwiki.wiki.views.index'),
+    (r'^/$', 'pydjangitwiki.wiki.views.index'),
     #(r'^/([^/?&#.]+)\.css$', # /:style.css
-    (r'^commit/([A-Fa-f0-9]{5,40})$', 'pydjangitwiki.wiki.indexviewcommit'), # commit/:sha
-    (r'^/?([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)?/archive$', 'pydjangitwiki.wiki.archive'), # /?:path?/archive #make zip file
-    (r'^/?([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)?/history$', 'pydjangitwiki.wiki.history'), # /?:path?/history
-    (r'^/?([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)?/diff$', 'pydjangitwiki.wiki.diff'), # /?:path?/diff
-    (r'^/([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/edit$', 'pydjangitwiki.wiki.edit'), # /:path/edit
-    (r'^/([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/upload$', 'pydjangitwiki.wiki.upload'), # /:path/upload
-    (r'^new$', 'pydjangitwiki.wiki.new'), # /new
-    (r'^upload$', 'pydjangitwiki.wiki.upload'), # /upload
-    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/new$', 'pydjangitwiki.wiki.new'), # /:path/new
-    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/upload$', 'pydjangitwiki.wiki.upload'), # /:path/upload
-    (r'^([A-Fa-f0-9]{5,40})$', 'pydjangitwiki.wiki.indexviewcommit'), # /:sha
-    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/([A-Fa-f0-9]{5,40})$', 'pydjangitwiki.wiki.viewcommit_for_file'), # /:path/:sha
-    (r'^changelog\.rss$', 'pydjangitwiki.wiki.changelog'), # /changelog.rss
-    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/changelog\.rss$', 'pydjangitwiki.wiki.changelog_for_file'), # /:path/changelog.rss
+    (r'^commit/([A-Fa-f0-9]{5,40})$', 'pydjangitwiki.wiki.views.indexviewcommit'), # commit/:sha
+    (r'^/?([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)?/archive$', 'pydjangitwiki.wiki.views.archive'), # /?:path?/archive #make zip file
+    (r'^/?([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)?/history$', 'pydjangitwiki.wiki.views.history'), # /?:path?/history
+    (r'^/?([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)?/diff$', 'pydjangitwiki.wiki.views.diff'), # /?:path?/diff
+    (r'^/([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/edit$', 'pydjangitwiki.wiki.views.edit'), # /:path/edit
+    (r'^/([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/upload$', 'pydjangitwiki.wiki.views.upload'), # /:path/upload
+    (r'^new$', 'pydjangitwiki.wiki.views.new'), # /new
+    (r'^upload$', 'pydjangitwiki.wiki.views.upload'), # /upload
+    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/new$', 'pydjangitwiki.wiki.views.new'), # /:path/new
+    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/upload$', 'pydjangitwiki.wiki.views.upload'), # /:path/upload
+    (r'^([A-Fa-f0-9]{5,40})$', 'pydjangitwiki.wiki.views.indexviewcommit'), # /:sha
+    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/([A-Fa-f0-9]{5,40})$', 'pydjangitwiki.wiki.views.viewcommit_for_file'), # /:path/:sha
+    (r'^changelog\.rss$', 'pydjangitwiki.wiki.views.changelog'), # /changelog.rss
+    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)/changelog\.rss$', 'pydjangitwiki.wiki.views.changelog_for_file'), # /:path/changelog.rss
     #the catch-all
-    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)$', 'pydjangitwiki.wiki.view'), # /:path
+    (r'^([\w:.+\-_\/](?:[\w:.+\-_\/ ]*[\w.+\-_\/])?)$', 'pydjangitwiki.wiki.views.view'), # /:path
 )
